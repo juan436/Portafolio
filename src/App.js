bbbab6c4594a106930d2dcec67571643041ed2ee
Juan Villegas <juancvillefer@gmail.com>
2023-11-21 16:05:31 -0400

    add Portfolio and SocialLinks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <div>
        <NavBar/>
        <Home/>
+       <SocialLinks/>
+       <Portfolio/>
     </div>
   );
 }
