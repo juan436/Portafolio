eb23760e02b8bf6af5ef2f2bb56fa3a112f699bd
juan <juancvillefer@gmail.com>
2024-05-16 10:50:16 -0400

    update home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import PerfilImage from "../assets/perfil.jpg";
+import PerfilImage from "../assets/perfil1.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 
 const Home = () => {
-
     return (
         <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-                <div className="flex flex-col justify-center h-full">
+                <div className="flex flex-col justify-center">
                     <h2 className="text-4xl sm:text-7xl font-bold text-white">
                         I´m a Back end Developer
                     </h2>
+
                     <p className="text-gray-500 py-4 max-w-md">
                         Student in the process of Telematics Engineering,
                         I have programming knowledge front-end and back-end,
@@ -32,16 +32,18 @@ const Home = () => {
                     </div>
                 </div>
                 <div>
-                    <img 
-                    src={PerfilImage} 
-                    alt="profile"
-                    className="rounded-2xl mx-auto w-2/3 md:w-full object-cover h-full" 
+                    <img
+                        src={PerfilImage}
+                        alt="profile"
+                        className="rounded-2xl mx-auto w-full md:max-w-lg object-cover h-auto md:h-max"
+                        style={{
+                            maskImage: "linear-gradient(to bottom, black 50%, transparent 92%)"
+                        }}
                     />
                 </div>
             </div>
         </div>
     );
 }
-
 
 export default Home;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       fontSize: {
         'font-sm': '12px',
-      }
+      },
     },
   },
   plugins: [],
