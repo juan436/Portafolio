65c01eb8d6053c90b3be24cedb3e0f90f3aaea1e
juan <juancvillefer@gmail.com>
2024-05-22 16:44:07 -0400

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,34 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const port = process.env.PORT || 4000;
 
-const { mongoose } = require('./database')
+const { mongoose } = require('./database');
 const app = express();
 
 // Settings
-app.set('port', port)
+app.set('port', port);
 app.use(cors());
 
-//Middlewares
+// Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
-
 // Routes
-const routerProject = require('./routers/projectsRouter')
-app.use('/api/projects',routerProject);
+const routerProject = require('./routers/projectsRouter');
+app.use('/api/projects', routerProject);
 
-//Prueba de funcionamiento
-app.get('/', (req, res)=>{
-    res.send('Prueba de funcionamiento de backend 🖥️')
+// Prueba de funcionamiento
+app.get('/', (req, res) => {
+    res.send('Prueba de funcionamiento de backend 🖥️');
 });
 
-
 app.listen(app.get('port'), () => {
-    console.log(`El servidor esta escuchando en el puerto ${app.get('port')}`);
-})
+    console.log(`El servidor está escuchando en el puerto ${app.get('port')}`);
+});
